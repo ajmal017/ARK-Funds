068bdd6688295f61b3ccf1c8fadc2d39045ce87e
iAmKalyanRevuru <kalyan@kiranum.com>
2021-05-04 18:39:11 +0530

    fix: files and class names renamed (ARK-67)

--- lib/models/holdings_screen_ListView.dart
+++ lib/models/holdings_screen_ListView.dart
@@ -1,16 +1,25 @@
-import 'package:arkfundsapp/models/holdings_model.dart';
 import 'package:arkfundsapp/providers/holdings_provider.dart';
 import 'package:arkfundsapp/screens/holdings_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_money_formatter/flutter_money_formatter.dart';
 
-class HoldingsModelGraph extends StatelessWidget {
-  final HoldingsObject holdingObj;
-  HoldingsModelGraph(this.holdingObj);
+class Holding {
+  final String company;
+  final String ticker;
+  final String date;
+  final List<dynamic> holding;
 
+  Holding({this.company, this.ticker, this.date, this.holding});
+}
+
+class HoldingsListView extends StatelessWidget {
+  final HoldingsObject holdingObj;
+  HoldingsListView(this.holdingObj);
   void selectHolding(BuildContext context, Holding holding) {
-    Navigator.of(context)
-        .pushNamed(HoldingsDetailScreen.routeName, arguments: holding);
+    Navigator.of(context).pushNamed(
+      HoldingsDetailScreen.routeName,
+      arguments: holding,
+    );
   }
 
   String moneyFormatter(double amount) {
@@ -21,6 +30,8 @@ class HoldingsModelGraph extends StatelessWidget {
   Widget build(BuildContext context) {
     List<dynamic> holdings = holdingObj.holdings;
     return ListView.builder(
+      // shrinkWrap: true,
+      // physics: NeverScrollableScrollPhysics(),
       itemBuilder: (ctx, index) {
         return Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -44,7 +55,7 @@ class HoldingsModelGraph extends StatelessWidget {
                     {
                       'title': 'Market Price',
                       'value': moneyFormatter(holdings[index]['marketValue'] /
-                          holdings[index]['numShares'])
+                          holdings[index]['numShares']),
                     },
                     {
                       'title': 'Market Value',
@@ -55,56 +66,56 @@ class HoldingsModelGraph extends StatelessWidget {
                 ),
               ),
               child: Container(
-                margin: EdgeInsets.all(5),
+                margin: EdgeInsets.zero,
                 width: double.infinity,
                 padding: EdgeInsets.only(
-                  left: 16,
-                  right: 12,
-                  top: 12,
+                  left: 20,
+                  right: 16,
+                  top: 8,
                   bottom: 8,
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.55,
-                      child: Text(
-                        holdings[index]['company'],
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'SF-Pro-Text',
-                        ),
-                        maxLines: 2,
-                      ),
-                    ),
-                    Spacer(),
-                    SizedBox(
-                      height: 15,
-                      width: MediaQuery.of(context).size.width * 0.35,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1.0,
-                          ),
-                          color: Color.fromRGBO(220, 220, 220, 1),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        alignment: Alignment.centerLeft,
-                        child: SizedBox(
-                          height: 15,
-                          width: MediaQuery.of(context).size.width *
-                              0.35 *
-                              (holdings[index]['weight'] / 100),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(5),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.65,
+                          child: Text(
+                            holdings[index]['company'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'SF-Pro-Text',
                             ),
+                            maxLines: 2,
                           ),
                         ),
-                      ),
+                        Text(
+                          holdings[index]['ticker'] != null
+                              ? holdings[index]['ticker']
+                              : '-',
+                          style: TextStyle(fontSize: 13),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          '${holdings[index]['weight']}%',
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'SF-Pro-Text',
+                            color: Color.fromRGBO(0, 0, 0, 0.4),
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color.fromRGBO(0, 0, 0, 0.5),
+                        ),
+                      ],
                     ),
                   ],
                 ),
