7d857533015c07b97d61297d65b315840bad4cb1
iAmKalyanRevuru <kalyan@kiranum.com>
2021-05-14 10:11:35 +0530

    fix: file names changed (ARK-61)

--- lib/screens/performance.dart
+++ lib/screens/performance.dart
@@ -1,4 +1,4 @@
-import 'package:arkfundsapp/providers/category.dart';
+import 'package:arkfundsapp/providers/fund_product_group_provider.dart';
 import 'package:arkfundsapp/providers/performance_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
